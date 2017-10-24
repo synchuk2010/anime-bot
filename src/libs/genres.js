c0a96062fae2e704a68d28fbda63328a64dd68d5
Alex Kozack <kozackunisoft@gmail.com>
2017-10-24 18:30:07 +0300

    All genres list

---
+++ src/libs/genres.js
@@ -0,0 +1,218 @@
+module.exports = [
+	{
+		'id':8,
+		'name': 'Drama',
+		'russian': 'Драма',
+		'trigger': /драм/i,
+	}, {
+		'id':11,
+		'name': 'Game',
+		'russian': 'Игры',
+		'trigger': /игр/i,
+	}, {
+		'id':40,
+		'name': 'Psychological',
+		'russian': 'Психологическое',
+		'trigger': /психологическ/i,
+	}, {
+		'id':2,
+		'name': 'Adventure',
+		'russian': 'Приключения',
+		'trigger': /приключения/i,
+	}, {
+		'id':19,
+		'name': 'Music',
+		'russian': 'Музыка',
+		'trigger': /музык/i,
+	}, {
+		'id':1,
+		'name': 'Action',
+		'russian': 'Экшен',
+		'trigger': /экшен/i,
+	}, {
+		'id':4,
+		'name': 'Comedy',
+		'russian': 'Комедия',
+		'trigger': /комеди/i,
+	}, {
+		'id':6,
+		'name': 'Demons',
+		'russian': 'Демоны',
+		'trigger': /демон/i,
+	}, {
+		'id':39,
+		'name': 'Police',
+		'russian': 'Полиция',
+		'trigger': /полиц/i,
+	}, {
+		'id':29,
+		'name': 'Space',
+		'russian': 'Космос',
+		'trigger': /космос/i,
+	}, {
+		'id':9,
+		'name': 'Ecchi',
+		'russian': 'Этти',
+		'trigger': /этти/i,
+	}, {
+		'id':10,
+		'name': 'Fantasy',
+		'russian': 'Фэнтези',
+		'trigger': /фэнтез/i,
+	}, {
+		'id':12,
+		'name': 'Hentai',
+		'russian': 'Хентай',
+		'trigger': /хентай/i,
+	}, {
+		'id':13,
+		'name': 'Historical',
+		'russian': 'Исторический',
+		'trigger': /истори/i,
+	}, {
+		'id':14,
+		'name': 'Horror',
+		'russian': 'Ужасы',
+		'trigger': /ужас/i,
+	}, {
+		'id':16,
+		'name': 'Magic',
+		'russian': 'Магия',
+		'trigger': /маг/i,
+	}, {
+		'id':18,
+		'name': 'Mecha',
+		'russian': 'Меха',
+		'trigger': /меха/i,
+	}, {
+		'id':20,
+		'name': 'Parody',
+		'russian': 'Пародия',
+		'trigger': /пароди/i,
+	}, {
+		'id':21,
+		'name': 'Samurai',
+		'russian': 'Самураи',
+		'trigger': /самур/i,
+	}, {
+		'id':22,
+		'name': 'Romance',
+		'russian': 'Романтика',
+		'trigger': /роман/i,
+	}, {
+		'id':23,
+		'name': 'School',
+		'russian': 'Школа',
+		'trigger': /школ/i,
+	}, {
+		'id':25,
+		'name': 'Shoujo',
+		'russian': 'Сёдзе',
+		'trigger': /(сёдзе|седзе)/i,
+	}, {
+		'id':27,
+		'name': 'Shounen',
+		'russian': 'Сёнен',
+		'trigger': /(сёнен|сенен)/i,
+	}, {
+		'id':28,
+		'name': 'Shounen Ai',
+		'russian': 'Сёнен Ай',
+		'trigger': /(сёнен|сенен) ?ай/i,
+	}, {
+		'id':30,
+		'name': 'Sports',
+		'russian': 'Спорт',
+		'trigger': /спорт/i,
+	}, {
+		'id':32,
+		'name': 'Vampire',
+		'russian': 'Вампиры',
+		'trigger': /вампир/i,
+	}, {
+		'id':33,
+		'name': 'Yaoi',
+		'russian': 'Яой',
+		'trigger': /яой/i,
+	}, {
+		'id':34,
+		'name': 'Yuri',
+		'russian': 'Юри',
+		'trigger': /юри/i,
+	}, {
+		'id':35,
+		'name': 'Harem',
+		'russian': 'Гарем',
+		'trigger': /гарем/i,
+	}, {
+		'id':36,
+		'name': 'Slice of Life',
+		'russian': 'Повседневность',
+		'trigger': /повседневность/i,
+	}, {
+		'id':42,
+		'name': 'Seinen',
+		'russian': 'Сейнен',
+		'trigger': /сейнен/i,
+	}, {
+		'id':43,
+		'name': 'Josei',
+		'russian': 'Дзёсей',
+		'trigger': /(дзёсей|дзесей)/i,
+	}, {
+		'id':37,
+		'name': 'Supernatural',
+		'russian': 'Сверхъестественное',
+		'trigger': /сверхъестественн/i,
+	}, {
+		'id':41,
+		'name': 'Thriller',
+		'russian': 'Триллер',
+		'trigger': /триллер/i,
+	}, {
+		'id':26,
+		'name': 'Shoujo Ai',
+		'russian': 'Сёдзе Ай',
+		'trigger': /(сёдзе|седзе) ?ай/i,
+	}, {
+		'id':24,
+		'name': 'Sci-Fi',
+		'russian': 'Фантастика',
+		'trigger': /фантаст/i,
+	}, {
+		'id':31,
+		'name': 'Super Power',
+		'russian': 'Супер сила',
+		'trigger': /супер сил/i,
+	}, {
+		'id':38,
+		'name': 'Military',
+		'russian': 'Военное',
+		'trigger': /(воен|войн)/i,
+	}, {
+		'id':7,
+		'name': 'Mystery',
+		'russian': 'Детектив',
+		'trigger': /детектив/i,
+	}, {
+		'id':15,
+		'name': 'Kids',
+		'russian': 'Детское',
+		'trigger': /дет/i,
+	}, {
+		'id':3,
+		'name': 'Cars',
+		'russian': 'Машины',
+		'trigger': /машин/i,
+	}, {
+		'id':17,
+		'name': 'Martial Arts',
+		'russian': 'Боевые искусства',
+		'trigger': /боевы\w* искусст\w*/i,
+	}, {
+		'id':5,
+		'name': 'Dementia',
+		'russian': 'Безумие',
+		'trigger': /безум/i,
+	},
+]
